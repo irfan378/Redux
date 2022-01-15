7131714ec7a11a4cc43e11d6d1c1653936701900
Irfan Farooq <irfandominate@gmail.com>
2022-01-15 11:20:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import Shop from './Components/Shop';
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <div className="container">
+        <Shop />
+      </div>
+    </>
+  );
+}
+
+
+
+export default App;
---
+++ src/Components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div>
+            <h2>Deposit/withdraw Money</h2>
+            <button className="btn btn-primary">+</button>
+            Update balance
+            <button className="btn btn-primary">-</button>
+        </div>
+    )
+}
+
+export default Shop
